974e6cd5f36bb6ef6ad27781e887af9996939511
Giovanna Carboni <giovannacarboniest@gmail.com>
2020-10-30 10:37:08 -0300

    aula quinta

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace estudos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+           
+           
+           
+           
+           
+           
+            //int [] a;
+            //a = new int [5]; //instanciei o array unidimensional
+           // a[0] = 5;
+            //a[3] = 3;
+           // Console.WriteLine(a[0]);
+            
+            //string[,] c = new string [4,4]; //array bidimensional
+
+            //double [][] d = new double [4][]; // isto e um array unidimensional, onde cada posicao tambem é outro array;
+
         }
     }
 }
